b01105446fed636e8b8992b5a7b20e7e3de5ea25
youknowwhorblx <119705696+youknowwhorblx@users.noreply.github.com>
2022-12-03 15:58:43 +1300

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,5 +1,6 @@
 -- insult (private) by youknowwho REWRITE 1.1
-local bedwarsLoad = "https://raw.githubusercontent.com/youknowwhorblx/insult/main/modules/skywars_by_voxels.lua"
+local bedWarsLoad = "https://raw.githubusercontent.com/youknowwhorblx/insult/main/modules/bedwars.lua"
+local skyWarsLoad = "https://raw.githubusercontent.com/youknowwhorblx/insult/main/modules/bedwars.lua"
 
 local placeID = game.PlaceId
 local function createinfo(this_title, this_notify, this_duration)
@@ -21,9 +22,13 @@ end
 -- bedwars
 print("PlaceID (Insult): "..placeID)
 
+if placeID == 6872274481 or 8444591321 or 8560631822 or 9903116309 then -- Skywars by Voxels
+	createinfo("Insult", 'Executing script for "BedWars", please wait...', 5)
+	loadstring(game:HttpGet(bedWarsLoad))()
+	createinfo("Insult", 'Finished loading for "BedWars". Have fun!', 5)
 if placeID == 8542259458 or 8542275097 or 8592115909 or 8768229691 or 8951451142 then -- Skywars by Voxels
 	createinfo("Insult", 'Executing script for "SkyWars", please wait...', 5)
-	loadstring(game:HttpGet(skyWarsLoad, true))()
+	loadstring(game:HttpGet(skyWarsLoad))()
 	createinfo("Insult", 'Finished loading for "SkyWars". Have fun!', 5)
 else
 	createinfo("Insult", "Unknown PlaceID. Insult will not execute.", 10)
