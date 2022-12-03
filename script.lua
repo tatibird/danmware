4bb81436507ca94dc28ad35e5f709fdb95b9a574
youknowwhorblx <119705696+youknowwhorblx@users.noreply.github.com>
2022-12-03 15:55:41 +1300

    Update script.lua

--- script.lua
+++ script.lua
@@ -11,14 +11,6 @@ local function createinfo(this_title, this_notify, this_duration)
 	})
 end
 
-if placeID == 8542259458 or 8542275097 or 8592115909 or 8768229691 or 8951451142 then -- Skywars by Voxels
-	createinfo("Insult", 'Executing script for "SkyWars", please wait...', 5)
-	loadstring(game:HttpGet(skyWarsLoad, true))()
-	createinfo("Insult", 'Finished loading for "SkyWars". Have fun!', 5)
-else
-	createinfo("Insult", "Unknown PlaceID. Insult will not execute.", 10)
-end
-
 if fluxus then
 	createinfo("Insult", 'Detected Fluxus', 5)
 elseif syn then
@@ -28,12 +20,11 @@ elseif KRNL_LOADED then
 end
 -- bedwars
 print("PlaceID (Insult): "..placeID)
-if placeID == 8560631822 or 8444591321 or 6872274481 then
+if placeID == 6872274481 then --8560631822 or 8444591321 or 
 	createinfo("Insult", 'Executing script for "BedWars", please wait...', 5)
 	loadstring(game:HttpGet(bedwarsLoad, true))()
 	createinfo("Insult", 'Finished loading for "BedWars". Have fun!', 5)
-end
-if placeID == 8542259458 or 8542275097 or 8592115909 or 8768229691 or 8951451142 then -- Skywars by Voxels
+elseif placeID == 8542259458 or 8542275097 or 8592115909 or 8768229691 or 8951451142 then -- Skywars by Voxels
 	createinfo("Insult", 'Executing script for "SkyWars", please wait...', 5)
 	loadstring(game:HttpGet(skyWarsLoad, true))()
 	createinfo("Insult", 'Finished loading for "SkyWars". Have fun!', 5)
